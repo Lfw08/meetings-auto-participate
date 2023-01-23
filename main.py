176302722eaaaa4d8b547234e9546e835d06ecce
Lfw <liufw666@outlook.com>
2023-01-23 19:09:55 +0800

    update

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def enter_meeting(id, ps_word, way):
     # 加入会议
     if way == 'Tencent':
         # 这里需要替换自己电脑中腾讯会议应用程序的位置
-        os.startfile(r"C:\Users\Public\Desktop\腾讯会议.lnk")
-        time.sleep(10)
+        os.startfile(r"C:\Program Files (x86)\Tencent\WeMeet\wemeetapp.exe")
+        time.sleep(20)
         click("enter_meeting.png")
         enter_wd(id)
         click("enter_meeting2.png")
